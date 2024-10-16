8fe49be26545da476477f0c4388b608c3ffdbd3a
Alex Beuscher <alexbeuscher@gmail.com>
2024-10-16 00:01:53 -0400

    Fiuxed build

--- build/scripts.js
+++ build/scripts.js
@@ -5,86 +5,67 @@ const sass = require("sass");
 const pugFunctionPlugin = require("./pug-function.js");
 const aliasPlugin = require("esbuild-plugin-alias");
 
-const BuildScripts = (eleventyConfig) => {
+const BuildScripts = (eleventyConfig, isWatchMode = false) => {
   const outputDir = path.join(__dirname, "../dist/scripts");
   if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir, { recursive: true });
   }
 
-  esbuild
-    .build({
-      entryPoints: ["./src/scripts/app.js", "./src/styles/theme.scss"],
-      bundle: true,
-      outdir: path.join(__dirname, "../dist"),
-      loader: {
-        ".js": "jsx",
-      },
-      plugins: [
-        aliasPlugin({
-          swiper: path.resolve(__dirname, "node_modules/swiper"),
-        }),
-        {
-          name: "sass-plugin",
-          setup(build) {
-            build.onLoad({ filter: /\.scss$/ }, async (args) => {
-              const result = sass.renderSync({
-                file: args.path,
-                importer: [
-                  function (url, prev, done) {
-                    if (url.startsWith("swiper")) {
-                      return { file: path.resolve("node_modules", url) };
-                    }
-                    return null;
-                  },
-                ],
-              });
-              return { contents: result.css.toString(), loader: "css" };
+  // Shared esbuild configuration
+  const esbuildConfig = {
+    entryPoints: ["./src/scripts/app.js", "./src/styles/theme.scss"],
+    bundle: true,
+    outdir: path.join(__dirname, "../dist"),
+    loader: {
+      ".js": "jsx",
+    },
+    plugins: [
+      aliasPlugin({
+        swiper: path.resolve(__dirname, "node_modules/swiper"),
+      }),
+      {
+        name: "sass-plugin",
+        setup(build) {
+          build.onLoad({ filter: /\.scss$/ }, async (args) => {
+            const result = sass.renderSync({
+              file: args.path,
+              importer: [
+                function (url, prev, done) {
+                  if (url.startsWith("swiper")) {
+                    return { file: path.resolve("node_modules", url) };
+                  }
+                  return null;
+                },
+              ],
             });
-          },
+            return { contents: result.css.toString(), loader: "css" };
+          });
         },
-        pugFunctionPlugin,
-      ],
-      minify: false,
-      platform: "browser",
-      sourcemap: true,
-    })
+      },
+      pugFunctionPlugin,
+    ],
+    minify: false,
+    platform: "browser",
+    sourcemap: true,
+  };
+
+  esbuild
+    .build(esbuildConfig)
     .then(() => {
       console.log("Build successful!");
 
-      esbuild
-        .context({
-          entryPoints: ["./src/scripts/app.js", "./src/styles/theme.scss"],
-          bundle: true,
-          outdir: path.join(__dirname, "../dist"),
-          loader: {
-            ".js": "jsx",
-          },
-          plugins: [
-            aliasPlugin({
-              swiper: path.resolve(__dirname, "node_modules/swiper"),
-            }),
-            {
-              name: "sass-plugin",
-              setup(build) {
-                build.onLoad({ filter: /\.scss$/ }, async (args) => {
-                  const result = sass.renderSync({ file: args.path });
-                  return { contents: result.css.toString(), loader: "css" };
-                });
-              },
-            },
-            pugFunctionPlugin,
-          ],
-          minify: false,
-          platform: "browser",
-          sourcemap: true,
-        })
-        .then((context) => {
-          context.watch();
-          console.log("Watching for changes...");
-        })
-        .catch((error) => {
-          console.error("Error watching files:", error);
-        });
+      // Only watch for changes in development mode
+      if (isWatchMode) {
+        esbuild
+          .context(esbuildConfig)
+          .then((context) => {
+            context.watch();
+            console.log("Watching for changes...");
+          })
+          .catch((error) => {
+            console.error("Error watching files:", error);
+          });
+      }
     })
     .catch((error) => {
       console.error("Build failed:", error);
